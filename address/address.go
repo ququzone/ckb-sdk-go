b6fd5890333a430ad584153bd5aa61df953f1300
ququzone <xueping.yang@gmail.com>
2019-12-06 01:30:54 +0800

    complete address parse

--- address/address.go
+++ address/address.go
@@ -2,6 +2,8 @@ package address
 
 import (
 	"encoding/hex"
+	"errors"
+	"strings"
 
 	"github.com/ethereum/go-ethereum/common"
 
@@ -11,10 +13,14 @@ import (
 )
 
 type Mode string
+type Type string
 
 const (
 	Mainnet Mode = "ckb"
 	Testnet Mode = "ckt"
+
+	TypeFull  Type = "Full"
+	TypeShort Type = "Short"
 
 	SHORT_FORMAT                 = "01"
 	FULL_DATA_FORMAT             = "02"
@@ -23,7 +29,13 @@ const (
 	CODE_HASH_INDEX_MULTISIG_SIG = "01"
 )
 
-func GenerateAddress(mode Mode, script *types.Script) (string, error) {
+type ParsedAddress struct {
+	Mode   Mode
+	Type   Type
+	Script *types.Script
+}
+
+func Generate(mode Mode, script *types.Script) (string, error) {
 	if script.HashType == types.HashTypeType && len(script.Args) == 20 {
 		if transaction.SECP256K1_BLAKE160_SIGHASH_ALL_TYPE_HASH == script.CodeHash.String() {
 			// generate_short_payload_singlesig_address
@@ -57,4 +69,58 @@ func generateFullPayloadAddress(hashType string, mode Mode, script *types.Script
 		return "", err
 	}
 	return bech32.Encode((string)(mode), data)
+}
+
+func Parse(address string) (*ParsedAddress, error) {
+	hrp, decoded, err := bech32.Decode(address)
+	if err != nil {
+		return nil, err
+	}
+	data, err := bech32.ConvertBits(decoded, 5, 8, false)
+	if err != nil {
+		return nil, err
+	}
+	payload := hex.EncodeToString(data)
+
+	var addressType Type
+	var script types.Script
+	if strings.HasPrefix(payload, "01") {
+		addressType = TypeShort
+		if CODE_HASH_INDEX_SINGLESIG == payload[2:4] {
+			script = types.Script{
+				CodeHash: types.HexToHash(transaction.SECP256K1_BLAKE160_SIGHASH_ALL_TYPE_HASH),
+				HashType: types.HashTypeType,
+				Args:     common.Hex2Bytes(payload[4:]),
+			}
+		} else {
+			script = types.Script{
+				CodeHash: types.HexToHash(transaction.SECP256K1_BLAKE160_MULTISIG_ALL_TYPE_HASH),
+				HashType: types.HashTypeType,
+				Args:     common.Hex2Bytes(payload[4:]),
+			}
+		}
+	} else if strings.HasPrefix(payload, "02") {
+		addressType = TypeFull
+		script = types.Script{
+			CodeHash: types.HexToHash(payload[2:66]),
+			HashType: types.HashTypeData,
+			Args:     common.Hex2Bytes(payload[66:]),
+		}
+	} else if strings.HasPrefix(payload, "04") {
+		addressType = TypeFull
+		script = types.Script{
+			CodeHash: types.HexToHash(payload[2:66]),
+			HashType: types.HashTypeType,
+			Args:     common.Hex2Bytes(payload[66:]),
+		}
+	} else {
+		return nil, errors.New("address type error:" + payload[:2])
+	}
+
+	result := &ParsedAddress{
+		Mode:   Mode(hrp),
+		Type:   addressType,
+		Script: &script,
+	}
+	return result, nil
 }
--- address/address_test.go
+++ address/address_test.go
@@ -9,19 +9,36 @@ import (
 	"github.com/ququzone/ckb-sdk-go/types"
 )
 
-func TestGenerateAddress(t *testing.T) {
+func TestGenerate(t *testing.T) {
 	script := &types.Script{
 		CodeHash: types.HexToHash("0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8"),
 		HashType: types.HashTypeType,
 		Args:     common.FromHex("0xedcda9513fa030ce4308e29245a22c022d0443bb"),
 	}
 
-	mnAddress, err := GenerateAddress(Mainnet, script)
+	mnAddress, err := Generate(Mainnet, script)
 
 	assert.Nil(t, err)
 	assert.Equal(t, "ckb1qyqwmndf2yl6qvxwgvyw9yj95gkqytgygwasshh9m8", mnAddress)
 
-	tnAddress, err := GenerateAddress(Testnet, script)
+	tnAddress, err := Generate(Testnet, script)
 	assert.Nil(t, err)
 	assert.Equal(t, "ckt1qyqwmndf2yl6qvxwgvyw9yj95gkqytgygwasdjf6hm", tnAddress)
+}
+
+func TestParse(t *testing.T) {
+	script := &types.Script{
+		CodeHash: types.HexToHash("0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8"),
+		HashType: types.HashTypeType,
+		Args:     common.FromHex("0xedcda9513fa030ce4308e29245a22c022d0443bb"),
+	}
+
+	mnAddress, err := Parse("ckb1qyqwmndf2yl6qvxwgvyw9yj95gkqytgygwasshh9m8")
+
+	assert.Nil(t, err)
+	assert.Equal(t, Mainnet, mnAddress.Mode)
+	assert.Equal(t, TypeShort, mnAddress.Type)
+	assert.Equal(t, script.CodeHash, mnAddress.Script.CodeHash)
+	assert.Equal(t, script.HashType, mnAddress.Script.HashType)
+	assert.Equal(t, script.Args, mnAddress.Script.Args)
 }
