3846c68420eb63356f683c079c7886f9344ae45e
ququzone <xueping.yang@gmail.com>
2019-12-11 11:30:37 +0800

    add multisig address generator

---
+++ address/multisig_address.go
@@ -0,0 +1,58 @@
+package address
+
+import (
+	"encoding/binary"
+	"errors"
+	"github.com/ququzone/ckb-sdk-go/crypto/blake2b"
+	"github.com/ququzone/ckb-sdk-go/transaction"
+	"github.com/ququzone/ckb-sdk-go/types"
+)
+
+func GenerateSecp256k1MultisigScript(requireN, threshold int, publicKeys [][]byte) (*types.Script, error) {
+	if requireN < 0 || requireN > 255 {
+		return nil, errors.New("requireN must ranging from 0 to 255")
+	}
+	if threshold < 0 || threshold > 255 {
+		return nil, errors.New("requireN must ranging from 0 to 255")
+	}
+	if len(publicKeys) > 255 {
+		return nil, errors.New("public keys size must be less than 256")
+	}
+	if len(publicKeys) < requireN || len(publicKeys) < threshold {
+		return nil, errors.New("public keys error")
+	}
+
+	var data []byte
+	data = append(data, 0)
+
+	b := make([]byte, 2)
+	binary.LittleEndian.PutUint16(b, uint16(requireN))
+	data = append(data, b[:1]...)
+
+	b = make([]byte, 2)
+	binary.LittleEndian.PutUint16(b, uint16(threshold))
+	data = append(data, b[:1]...)
+
+	b = make([]byte, 2)
+	binary.LittleEndian.PutUint16(b, uint16(len(publicKeys)))
+	data = append(data, b[:1]...)
+
+	for _, pub := range publicKeys {
+		hash, err := blake2b.Blake160(pub)
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, hash...)
+	}
+
+	args, err := blake2b.Blake160(data)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.Script{
+		CodeHash: types.HexToHash(transaction.SECP256K1_BLAKE160_MULTISIG_ALL_TYPE_HASH),
+		HashType: types.HashTypeType,
+		Args:     args,
+	}, nil
+}
